cc03c3087248ad812eddb05751154b8843adf89b
Caio <caio-matheus2050@hotmail.com>
2024-12-05 15:57:48 -0300

    First commit

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,28 @@
 describe('Orange HRM Tests', () => {
+  
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    submitField : "[type='submit']",
+    sectionTitleTopBar: ".oxd-text--h6.oxd-topbar-header-breadcrumb-module",
+    wrontCredentialAlert : "[role='alert']"
+    
+  }
+
   it('Login with Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get("[type='submit']").type('click')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.submitField).type('click')
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
   it('Login with Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('test')
-    cy.get("[name='password']").type('test')
-    cy.get("[type='submit']").type('click')
-    cy.get("[role='alert']")
+    cy.get(selectorsList.usernameField).type('test')
+    cy.get(selectorsList.passwordField).type('test')
+    cy.get(selectorsList.submitField).type('click')
+    cy.get(selectorsList.wrontCredentialAlert)
     
   })
 })
