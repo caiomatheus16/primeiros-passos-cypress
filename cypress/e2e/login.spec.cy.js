45d85ec62002ba28935ff359c8b11bae313ddc72
Caio <caio-matheus2050@hotmail.com>
2024-12-05 14:40:33 -0300

    First commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('Orange HRM Tests', () => {
+  it('Login with Sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").type('click')
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+  it('Login with Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('test')
+    cy.get("[name='password']").type('test')
+    cy.get("[type='submit']").type('click')
+    cy.get("[role='alert']")
+    
+  })
+})
