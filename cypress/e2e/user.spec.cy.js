13a522828494944fee42697a836e5d483a0e0036
Caio <caio-matheus2050@hotmail.com>
2024-12-11 15:14:24 -0300

    First Commit

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,20 +7,28 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     submitField : "[type='submit']",
     dashboardGrid :".orangehrm-dashboard-grid",
-    wrongCredentialAlert : "[role='alert']"
-  
-  }
+    wrongCredentialAlert : "[role='alert']",
+    myInfoButton : '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField : "[name='firstName']",
+    lastNameField : "[name='lastName']",
+    genericNameField : ".oxd-input--active",
+    middleNameField : "[name='middleName']"
+}
 
- 
-
-  it('Login with Sucess', () => {
+  it.only('User info Update - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.submitField).type('click')
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
-  })
+    cy.get (selectorsList.myInfoButton).type('click')
+    cy.get (selectorsList.firstNameField).type('FirstNameTest')
+    cy.get (selectorsList.lastNameField).type('LastNameField')
+    cy.get (selectorsList.middleNameField).type('MiddleNameField')
+    cy.get (selectorsList.genericNameField).eq(4).type('otherIdTest')
+    cy.get (selectorsList.genericNameField).eq(5).type ('DriverLicenseTest')
+   })
   it('Login with Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
