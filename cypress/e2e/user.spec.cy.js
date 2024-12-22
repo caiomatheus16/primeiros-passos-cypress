761864cdac5463c10ba921d537d9cc9b76b082da
Caio <caio-matheus2050@hotmail.com>
2024-12-22 16:25:01 -0300

    Spec Updates

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage.js'
 import MenuPage from '../pages/menuPage.js'
 import MyInfoPage from '../pages/myInfoPage.js'
 
+const Chance = require('chance')
+const chance = new Chance()
+
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -16,9 +19,10 @@ describe('Orange HRM Tests', () => {
     loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)
     dashboardPage.checkDashBoardPage()
     menuPage.acessMyInfo()
-    myInfoPage.fillPersonalDetails('First name', 'Last name', 'nickname'),
-    myInfoPage.fillEmployeeDetails('EmployeeID', 'OtherId'),
+    myInfoPage.fillPersonalDetails(chance.first(), chance.last(), chance.string(), ),
+    myInfoPage.fillEmployeeDetails('32124456', '123456', chance.integer(), '2024-13-12', '2024-12-05' ),
     myInfoPage.fillStatus()
+    myInfoPage.saveform()
     
     })
  
--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -7,37 +7,54 @@ class MyInfoPage{
             middleNameField : "[name='middleName']",
             updateField:".oxd-select-text-input",
             submitField : "[type='submit']",
+            genericComboBox : ".oxd-select-text--arrow",
+            secondItemComboBox : ".oxd-select-dropdown > :ntd-child(2)",
+            thirdItemComboBox : ".oxd-select-dropdown > :ntd-child(3)",
+            dateCloseButton : ".--close",
+            
+
         }
         return selectors;
     }
 
     fillPersonalDetails (firstName,lastName,nickName) {
-        cy.get(this.selectorsList().firstNameField).type(firstName)
-        cy.get(this.selectorsList().lastNameField).type(lastName)
-        cy.get(this.selectorsList().middleNameField).type(nickName)
+        cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+        cy.get(this.selectorsList().middleNameField).clear().type(nickName)
     
 }
 
-fillEmployeeDetails (otherId,DriversLicenseDate) { 
-    cy.get(this.selectorsList().genericNameField).eq(4).type(otherId)
-    cy.get(this.selectorsList().genericNameField).eq(5).type (DriversLicenseDate)
+fillEmployeeDetails (EmployeeID,OtherID, driversLicenseNumber,expiryDate,dateAniversary,){ 
+    
+    cy.get(this.selectorsList().genericNameField).eq(3).clear().type(EmployeeID)
+    cy.get(this.selectorsList().genericNameField).eq(4).clear().type(OtherID)
+    cy.get(this.selectorsList().genericNameField).eq(5).clear().type(driversLicenseNumber)
+    
+    cy.get(this.selectorsList().genericNameField).eq(6).clear().type(expiryDate)
+    // cy.get(this.selectorsList().dataCloseButton).click()
+    cy.get(this.selectorsList().genericNameField).eq(8).clear().type(dateAniversary)
+    
+    
+
 }
 
+
+saveform(){
+    cy.get(this.selectorsList().submitField).eq(0).click({force : true})
+    cy.get('body').should('contain', 'Sucessfully Updated')
+    cy.get('.oxd-toast-close')
+
+}
 fillStatus() {
     cy.get(this.selectorsList().updateField).eq(0).type('click')
-    cy.contains('Albanian').click()
+    cy.contains('American').click()
     cy.get(this.selectorsList().updateField).eq(1).type('click')
     cy.contains('Married').click()
     cy.get(this.selectorsList().updateField).eq(2).type('click')
     cy.contains('B-').click()
 }
 
-// saveform() {
-//     cy.get(this.selectorsList().submitField).eq(0).click({force : true})
-//     cy.get('body').should('contain', 'Sucessfully Updated')
-//     cy.get('.oxd-toast-close')
-    
-// }
+
 
 }
 export default MyInfoPage;
